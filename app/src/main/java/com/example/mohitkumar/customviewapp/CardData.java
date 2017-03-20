561f23c17266670dc3d933ba34a66baca246298a
Mohit Kumar <mohitkumar@Mohits-MacBook-Pro.local>
2017-03-20 10:28:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mohitkumar/customviewapp/CardData.java
@@ -0,0 +1,8 @@
+package com.example.mohitkumar.customviewapp;
+
+/**
+ * Created by mohitkumar on 20/03/17.
+ */
+
+public class CardData {
+}
---
+++ app/src/main/java/com/example/mohitkumar/customviewapp/ViewPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.example.mohitkumar.customviewapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by mohitkumar on 20/03/17.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
